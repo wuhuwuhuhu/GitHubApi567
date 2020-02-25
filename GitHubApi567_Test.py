3441a2e6d74901c859c8e9d3bd0d86b85cde9bc7
wuhuwuhuhu <wuhd1234@gmail.com>
2020-02-25 13:35:43 -0500

    Update GitHubApi567_Test.py

--- GitHubApi567_Test.py
+++ GitHubApi567_Test.py
@@ -1,12 +1,23 @@
 """This is test file for GitHubApi567
     Written by Haodong Wu
 """
-from GitHubApi567 import repository_get
+from GitHubApi567 import repository_get, commit_get
 import unittest
+from unittest.mock import Mock,patch
+import json
+class TestGitHubApi567(unittest.TestCase):
+    
+    @patch('requests.get')
+    def test_repository_get(self, mockedReq):
+        #side_effects = ['[{"name": "hellogitworld"}]'.encode]
+        #mockedReq.side_effect = ['[{"name": "hellogitworld"}]','[{"commit":{}},{"commit":{}}]']
+        mockedReq.return_value.text = '[{"name": "hellogitworld"}]'
+        self.assertEqual(repository_get('richkempinski'), [('hellogitworld', 1)])
 
-class GitHubApi567(unittest.TestCase):
-    def test_repository_get(self):
-        self.assertEqual(repository_get('richkempinski'), [('hellogitworld', 30), ('helloworld', 6), ('Mocks', 10), ('Project1', 2), ('threads-of-life', 1)])
+    @patch('requests.get')
+    def test_commit_get(self,mockedReq):
+        mockedReq.return_value.text = '[{"commit":{}},{"commit":{}}]'
+        self.assertAlmostEqual(commit_get('richkempinski','hellogitworld'),2)
 
 if __name__ == "__main__":
     unittest.main(exit = False, verbosity= 2)
